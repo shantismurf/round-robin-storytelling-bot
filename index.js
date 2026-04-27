37a4b5b1a9ebf4a6ff6086a0689d140789dfef12
shantismurf <leeannsziegler@gmail.com>
2026-04-27 11:52:50 -0500

    feat: schedule roundup jobs on bot ready

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { StoryBot, updateStoryStatusMessage } from './storybot.js';
 import { loadConfig, DB, getConfigValue, isGuildConfigured, setTestMode, log } from './utilities.js';
 import { main as deploy } from './deploy.js';
 import { startJobRunner } from './job-runner.js';
+import { scheduleAllRoundupJobs } from './story/roundup.js';
 import fs from 'fs';
 
 /**
@@ -100,6 +101,7 @@ async function main() {
     await bot.start();
     await loadCommands('./commands');
     startJobRunner(connection, client);
+    scheduleAllRoundupJobs(connection);
     refreshAllStatusMessages(connection, client);
   });
   function formatCommandLog(interaction) {
